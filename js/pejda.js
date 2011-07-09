f3a2503ac04d3d93d8cb953ec5a4a20fef3c56d5
Kyle Welsby <kyle@mekyle.com>
2011-07-09 01:57:40 +0100

    added more aewsomeness to the actions of the page.

--- js/pejda.js
+++ js/pejda.js
@@ -10,9 +10,31 @@ function detect_service(url) {
     return label;
 }
 
+function show_section(element) {
+    console.log(element.index());
+    $("#content .wrap:visible:not(#"+element.attr("id")+")").animate({
+        left:"-100%",
+        opacity:0
+    },1000,'swing',function(){
+        $(this).hide();
+        $(this).css({
+            left:"100%"
+        });
+    });
+    element.not(":visible").show().css({
+        left: '100%',
+        opacity: 0
+    }).animate({
+        left: 0,
+        opacity: 1
+    },1000,'swing',function(){
+        // $(this).hide();
+    });
+}
+
 $(document).ready(function() {
     (function() {
-        var schemes = ["pink", "blue", "green"],
+        var schemes = ["black", "pink", "blue", "green"],
         div = $("<div>", {
             'class': 'colorSchemes'
         });
@@ -22,6 +44,7 @@ $(document).ready(function() {
             // var scheme = value;
             link = $("<a>", {
                 text: scheme,
+                'class': scheme + " ir",
                 href: "#" + scheme
             }).bind("click",
             function(e) {
@@ -35,30 +58,95 @@ $(document).ready(function() {
         $("body").append(div);
     })();
 
-    $("#wrap").empty();
-    $.each(tumblr_api_read.posts,
-    function() {
-        var element = $("<article>", {
-            id: "post_" + this.id
-        }),
-        figure = $("<figure>"),
-        figcaption = $("<figcaption>", {
-            html: this['photo-caption']
-        }),
-        image = $("<img>", {
-            src: this['photo-url-250']
-        }),
-        photoURL = this['photo-link-url'],
-        a = $("<a>", {
-            href: photoURL,
-            'class': detect_service(photoURL).toLowerCase(),
-            text: detect_service(photoURL)
+    (function() {
+        $("a[href='#work']").click(function() {
+            show_section($("#portfolio"));
         });
-        figure.append(image);
-        figcaption.append(a);
-        figure.append(figcaption);
-        element.append(figure);
+        $("a[href='#mac']").click(function() {
+            show_section($("#portfolio_mac"));
+        });
+        $("a[href='#iphone']").click(function() {
+            show_section($("#portfolio_iphone"));
+        });
+        $("a[href='#contact']").click(function() {
+            show_section($("#contact"));
+        });
+    })();
 
-        $("#wrap").append(element);
-    });
+    (function() {
+        var portfolio = $("<div>",{
+            id: 'portfolio',
+            'class':'wrap'
+        }),
+        portfolio_mac = $("<div>", {
+            id: 'portfolio_mac',
+            'class': 'wrap'
+        }),
+        portfolio_iphone = $("<div>", {
+            id: 'portfolio_iphone',
+            'class': 'wrap'
+        });
+        if (window.tumblr_api_read) {
+            $.each(tumblr_api_read.posts,
+            function() {
+                var element = $("<article>", {
+                    id: "post_" + this.id
+                }),
+                figure = $("<figure>"),
+                figcaption = $("<figcaption>", {
+                    html: this['photo-caption']
+                }),
+                image = $("<img>", {
+                    src: this['photo-url-250']
+                }),
+                photoURL = this['photo-link-url'],
+                a = $("<a>", {
+                    href: photoURL,
+                    'class': detect_service(photoURL).toLowerCase(),
+                    text: detect_service(photoURL)
+                });
+                figure.append(image);
+                figcaption.append(a);
+                figure.append(figcaption);
+                element.append(figure);
+                $.each(this.tags,
+                function(i, value) {
+                    if(value === "portfolio"){
+                        portfolio.append(element);
+                    }else if (value === "mac"){
+                        portfolio_mac.append(element);
+                    }else if (value === "iphone"){
+                        portfolio_iphone.append(element);
+                    }
+                });
+            });
+            $("#content").append(portfolio);
+            $("#content").append(portfolio_mac);
+            $("#content").append(portfolio_iphone);
+        } else {
+            (function() {
+                var notice = $("<div>", {
+                    'class': 'notice'
+                }),
+                header = $("<h2>", {
+                    text: "Oh fuck!"
+                }),
+                message = $("<p>", {
+                    text: "Tumblr has just shit it's self. My awesome content will not be viewable just yet. Bring yo' self back later."
+                });
+                notice.css({
+                    right: -notice.width()
+                });
+                notice.append(header);
+                notice.append(message);
+                $("body").append(notice);
+                notice.animate({
+                    right: 0
+                },
+                2000, 'swing');
+                
+                show_section($("#contact"));
+            })();
+        }
+    })();
 });
